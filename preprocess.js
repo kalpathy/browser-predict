06f5c955a1dda866268097b1adf1d0b22c031dbd
kalpathy <kalpathy@nmr.mgh.harvard.edu>
2019-02-24 19:02:16 -0500

    extracted predict function

--- preprocess.js
+++ preprocess.js
@@ -6,3 +6,34 @@ let testTf = function() {
   a.print();
   return a;
 }
+
+
+let preprocess = function(imgElement) {
+
+  const startTime = performance.now();
+  let w = imgElement.width
+  let h = imgElement.height
+  if (w < h) {
+    imgElement.width = IMAGE_SIZE
+    imgElement.height = Math.floor(IMAGE_SIZE*h/w)
+  } else {
+    imgElement.height = IMAGE_SIZE
+    imgElement.width = Math.floor(IMAGE_SIZE*w/h)
+  }
+
+  console.log("img wxh: " + w + ", " + h + " => " + imgElement.width + ", " + imgElement.height)
+
+  let img = tf.fromPixels(imgElement).toFloat();
+
+  let normalized = img.div(tf.scalar(255));
+
+  let meanImg = normalized.mean(2)
+  let hOffset = Math.floor(img.shape[1]/2 - IMAGE_SIZE/2)
+  let wOffset = Math.floor(img.shape[0]/2 - IMAGE_SIZE/2)
+
+  let cropImg = meanImg.slice([wOffset,hOffset],[IMAGE_SIZE,IMAGE_SIZE])
+
+  console.log("Prepared input image " + Math.floor(performance.now() - startTime) + "ms");
+  
+  return cropImg;
+}
