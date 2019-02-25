bb92d0f8772d80b2ab7285fc70a0f3d23f4caa6a
kalpathy <kalpathy@nmr.mgh.harvard.edu>
2019-02-24 20:15:24 -0500

    display logits in table on page

---
+++ drawTable.js
@@ -0,0 +1,33 @@
+
+function buildTable(labels, logits, norms) {
+		var table = d3.select('body').append('table')
+		var thead = table.append('thead')
+		var	tbody = table.append('tbody');
+
+    columns = ['pathology', 'prediction'];
+		// append the header row
+		thead.append('tr')
+		  .selectAll('th')
+		  .data(columns).enter()
+		  .append('th')
+		    .text(function (column) { return column; });
+
+		// create a row for each object in the data
+		var rows = tbody.selectAll('tr')
+		  .data(labels)
+		  .enter()
+		  .append('tr');
+
+		// create a cell in each row for each column
+		var cells = rows.selectAll('td')
+		  .data(function (row, irow) {
+		    return columns.map(function (column) {
+		      return {column: column, value: column === 'pathology' ? labels[irow] : logits[irow]};
+		    });
+		  })
+		  .enter()
+		  .append('td')
+		    .text(function (d) { return d.value; });
+
+	  return table;
+	}
